14e3bfe9fe55e8637259be8bdc336ac72be48ac0
Roman Ohiienko <109099364+ohiienko-r@users.noreply.github.com>
2024-05-10 13:26:59 +0300

    Update src/firebase/auth/useAuth.tsx
    Co-authored-by: Nick Yamnyk <38811365+yamnyk@users.noreply.github.com>

--- src/firebase/auth/useAuth.tsx
+++ src/firebase/auth/useAuth.tsx
@@ -3,7 +3,11 @@ import { getCurrentUser, auth } from './index'
 import { User, onAuthStateChanged } from 'firebase/auth'
 
 const useAuth = () => {
-	const [currentUser, setCurrentUser] = useState<User | null>(getCurrentUser())
+	const [currentUser, setCurrentUser] = useState<User | null>(null)
+	
+	useEffect(() => {
+	  setCurrentUser(getCurrentUser())
+	},[])
 	onAuthStateChanged(auth, user => setCurrentUser(user))
 	return { currentUser }
 }
