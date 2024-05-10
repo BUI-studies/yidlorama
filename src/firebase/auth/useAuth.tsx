e0d1e95b86fbdad6a3d278458cef5041480359a9
Roman Ohiienko <ohiienko.r@gmail.com>
2024-05-10 14:01:50 +0300

    modify useAuth hook

--- src/firebase/auth/useAuth.tsx
+++ src/firebase/auth/useAuth.tsx
@@ -1,15 +1,17 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { getCurrentUser, auth } from './index'
 import { User, onAuthStateChanged } from 'firebase/auth'
 
 const useAuth = () => {
-	const [currentUser, setCurrentUser] = useState<User | null>(null)
-	
+	const [currentUser, setCurrentUser] = useState<User | null>(getCurrentUser())
+
 	useEffect(() => {
-	  setCurrentUser(getCurrentUser())
-	},[])
-	onAuthStateChanged(auth, user => setCurrentUser(user))
-	return { currentUser }
+		const unsubscribe = onAuthStateChanged(auth, user => setCurrentUser(user))
+
+		return () => unsubscribe()
+	}, [])
+
+	return currentUser
 }
 
 export default useAuth
