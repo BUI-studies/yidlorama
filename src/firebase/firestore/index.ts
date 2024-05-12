f1e964cae66d7c58c47d3d07cdb85bbf81bb4cef
Roman Ohiienko <ohiienko.r@gmail.com>
2024-05-12 11:05:59 +0300

    Complete getUserRoleData function

--- src/firebase/firestore/index.ts
+++ src/firebase/firestore/index.ts
@@ -1,4 +1,4 @@
-import { getFirestore, addDoc, collection } from 'firebase/firestore'
+import { getFirestore, addDoc, collection, query, where, getDocs } from 'firebase/firestore'
 import { app } from '../main'
 import { UserRoleData } from './types'
 
@@ -13,6 +13,27 @@ const db = getFirestore(app)
 export const setNewUserRoleData = async ({ role, uid }: UserRoleData): Promise<void> => {
 	try {
 		await addDoc(collection(db, 'users'), { role: role, uid: uid })
+	} catch (error) {
+		throw new Error(`AN ERROR OCCURED: ${error}`)
+	}
+}
+
+/**
+ * Function gets user role data from firestore according to user's uid.
+ * 
+ * @param {string} uid - current user's uid; 
+ * @returns a current user's role in the system.
+ */
+export const getUserRoleData = async (uid: string): Promise<string | undefined> => {
+	let currentUserRole
+	const q = query(collection(db, 'users'), where('uid', '==', uid))
+	try {
+		const querySnapshot = await getDocs(q)
+		querySnapshot.forEach(doc => {
+			const data = doc.data()
+			currentUserRole = data.role
+		})
+		return currentUserRole
 	} catch (error) {
 		throw new Error(`AN ERROR OCCURED: ${error}`)
 	}
