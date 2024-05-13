55c1a6800989dc76f4d17d7a5e8d8bdfcab51fb8
AKot1988 <kulibabenko@ukr.net>
2024-05-13 17:21:06 +0300

    universal table component styles

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -1,22 +1,9 @@
-import { UniversalTable } from "@/components"
-
 const Menu = () => {
 	return (
-	<>
-		<h2>Menu</h2>
-		
-		<UniversalTable
-			data={[
-				{ id: 1, name: 'Pizza', price: 10 },
-				{ id: 2, name: 'Burger', price: 5 },
-				{ id: 3, name: 'Coke', price: 2 },
-			]}
-			headers={[
-				{ title: 'ID', property: 'id' },
-				{ title: 'Name', property: 'name' },
-				{ title: 'Price', property: 'price' },
-			]}/>
-	</>)
+		<>
+			<h2>Menu</h2>
+		</>
+	)
 
 }
 
