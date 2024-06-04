583880ec64ec582ddfc607e16ec8b6cbf6f374b7
AKot1988 <kulibabenko@ukr.net>
2024-06-04 15:41:50 +0300

    lil change in jsx order

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -9,17 +9,16 @@ import classes from './Card.module.scss';
  }
 
 const Card: FC<CardProps> = ({title, data}) => {
-      const categoryItems = data.map((item) => 
-        <div className={classes.cardListItem}>
-          <p className={classes.cardListItemName}>{item.name}</p>
-          <p className={classes.cardListItemValue}>{item.price}</p>
-        </div>
-      )
       return (
         <div className={classes.card}>
           <h2 className={classes.cardTitle}>{title}</h2>
           <div className={classes.cardList}>
-            {categoryItems}
+            {data.map((item) => 
+              <div className={classes.cardListItem}>
+                <p className={classes.cardListItemName}>{item.name}</p>
+                <p className={classes.cardListItemValue}>{item.price}</p>
+              </div>
+            )}
           </div>
         </div>
       )
